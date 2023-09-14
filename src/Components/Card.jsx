f9da0248611e54ca1eb03a28d38f2cdf238829f1
nayon117 <hasibulhasannayon260@gmail.com>
2023-09-14 20:09:17 +0600

    add dynamic card and implement dynamic funtionality on card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,16 +1,33 @@
- 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Card = () => {
-    return (
-        <div>
-            
+const Card = ({ course }) => {
+  return (
+    <div>
+      <div className="card bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src={course.img}
+            alt="course image"
+          />
+        </figure>
+        <div className="card-body ">
+                  <h2 className="card-title">{course.title }</h2>
+                  <p>{course.details}</p>
+                  <div className="flex justify-between">
+                      <p>$ Price:{course.price }</p>
+                      <p>Credit: {course.credit }hr</p>
+                  </div>
+          <div className="card-actions  ">
+            <button className="btn btn-block bg-[#2F80ED] text-white font-semibold">Select</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 Card.propTypes = {
-    
+  course: PropTypes.array.isRequired,
 };
 
 export default Card;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,29 @@
 import PropTypes from "prop-types";
+import { useEffect } from "react";
+import { useState } from "react";
+import Card from "./Card";
 
 const Home = () => {
+
+    const [courses,setCourses] = useState([])
+
+    useEffect(() => {
+        fetch("./data.json")
+        .then(res=>res.json())
+        .then(data=>setCourses(data))
+    },[])
+
+
   return (
     <div className="flex flex-col md:flex-row">
       {/* card section  */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"> 
 
+          {
+               courses.map(course=><Card key={course.id} course={course} ></Card>)
+          }
+
+          </div>
       {/* cart section  */}
       <div></div>
     </div>
